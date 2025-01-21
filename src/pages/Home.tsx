fe0c248a1587d00b21ce5aab33ae2b005e3d492b
Ony <onyrakoto27@gmail.com>
2025-01-21 14:40:34 +0300

    remove test code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,3 @@
-import { useApiClient } from "@/hooks";
-
 export function Home() {
-  const api = useApiClient();
-
-  const { loading, data, errors } = api.useTracks();
-
-  console.log({
-    loading,
-    data,
-    errors,
-  });
-
   return <div>Home</div>;
 }
