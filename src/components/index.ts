a617e909e97884756edff8e5f555e56c4ccd1da1
Ony <onyrakoto27@gmail.com>
2025-02-07 11:11:43 +0300

    Fix components name

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export * from "./player";
-export * from "./app=title";
+export * from "./app-title";
 export * from "./page-title";
 export * from "./app-sidebar";
 export * from "./track-load-toast";
