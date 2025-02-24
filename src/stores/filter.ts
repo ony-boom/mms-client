7b333078b544b029b9a04a60eb77f9dae9bdeb17
Ony <onyrakoto27@gmail.com>
2025-02-24 15:29:14 +0300

    persist sort state

--- src/stores/filter.ts
+++ src/stores/filter.ts
@@ -1,7 +1,8 @@
 import { create } from "zustand/react";
+import { persist } from "zustand/middleware";
 import { GetTrackSortByInput, GetTrackWhereInput } from "@/api/Api.ts";
 
-export const useFilterStore = create<{
+interface FilterStore {
   query?: GetTrackWhereInput;
   sort?: GetTrackSortByInput;
   setFilter: (filter: {
@@ -12,12 +13,26 @@ export const useFilterStore = create<{
   setSort: (sort?: GetTrackSortByInput) => void;
   openSearchComponent: boolean;
   setOpenSearchComponent: (open: boolean) => void;
-}>((set) => ({
-  openSearchComponent: false,
-  setOpenSearchComponent: (open) => set({ openSearchComponent: open }),
-  query: undefined,
-  sort: undefined,
-  setSort: (sort) => set({ sort }),
-  setQuery: (query) => set({ query }),
-  setFilter: (filter) => set(filter),
-}));
+}
+
+export const useFilterStore = create<FilterStore>()(
+  persist(
+    (set) => ({
+      openSearchComponent: false,
+      setOpenSearchComponent: (open) => set({ openSearchComponent: open }),
+      query: undefined,
+      sort: undefined,
+      setSort: (sort) => set({ sort }),
+      setQuery: (query) => set({ query }),
+      setFilter: (filter) => set(filter),
+    }),
+    {
+      name: "filter",
+      partialize(state) {
+        return {
+          sort: state.sort,
+        };
+      },
+    },
+  ),
+);
